65beb135d76ef4c3b6fd31f2d46099b4a885f773
krana <kranat10@gmail.com>
2025-02-03 22:37:36 -0600

    changed localhost connection in cypress.config.ts to the secret deply hook provided in render

--- cypress.config.ts
+++ cypress.config.ts
@@ -12,7 +12,9 @@ export default defineConfig({
   },
 
   e2e: {
-    baseUrl: 'http://localhost:3001',
+    // Going to attempt to replace this Localhost address with the mongodb address
+    // baseUrl: 'http://localhost:3001',
+    baseUrl: 'https://api.render.com/deploy/srv-cug2n41opnds73bd2uh0?key=x9avBADtU9c',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
